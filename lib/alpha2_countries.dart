28b1aed9b64ed60e2fe57d3b2b1f4ab27c91660c
Vadim Gulyakin <vgulyakin@rightandabove.com>
2021-06-11 12:54:16 +0300

    return null when country is not resolved

--- lib/alpha2_countries.dart
+++ lib/alpha2_countries.dart
@@ -3,6 +3,7 @@ library alpha2_countries;
 import 'dart:convert';
 
 import 'package:alpha2_countries/alpha2_countries_data.dart';
+import 'package:collection/collection.dart';
 
 class Countries {
   static final Countries _singleton = new Countries._internal();
@@ -21,14 +22,14 @@ class Countries {
     _data = data.map((e) => Country.fromJson(e)).toList();
   }
 
-  String resolveName(String code) => resolveCountryByCode(code).name;
+  String? resolveName(String code) => resolveCountryByCode(code)?.name;
 
-  String resolveCode(String country) => resolveCountryByName(country).code;
+  String? resolveCode(String country) => resolveCountryByName(country)?.code;
 
-  Country resolveCountryByName(String name) => _data.firstWhere(
+  Country? resolveCountryByName(String name) => _data.firstWhereOrNull(
       (element) => element.name.toLowerCase() == name.toLowerCase());
 
-  Country resolveCountryByCode(String code) => _data.firstWhere(
+  Country? resolveCountryByCode(String code) => _data.firstWhereOrNull(
       (element) => element.code.toLowerCase() == code.toLowerCase());
 
   List<Country> get countries => _data;
